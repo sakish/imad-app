5c48ec8abcb90d104ff4f2765bbea5b2b9e6bcc4
sakish <sarajagop@gmail.com>
2017-08-23 00:36:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+function hash(input, salt) {
+    //how do we create a crypto
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input', function (req, res)) {
+    var hashedString = hash(req.params.input, 'this is some random string');
+    res.send(hashedString);
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
