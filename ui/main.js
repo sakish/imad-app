6eeb1a193da97fcef7ba3a3e389d7e3144b47109
sakish <sarajagop@gmail.com>
2017-08-27 02:10:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,24 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML= "New Value";
-var img = document.getElementById('madi');
-//move the image.
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginRight + 10;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function (){
-    var interval = setInterval(moveRight, 100);
+//submit username/password to login
+
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for (var i=0; i< names.length;i++) {
+                  list +='<li>'+ names[i] + '</li>';
+                }
+                var ul = document.getElementById('nonelist');
+                ul.innerHTML = list;
+            }
+        }
+    };
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    request.open('POST', 'http://sarajagop.imad.hasura-app.io/submit-none?none=' + name, true);
+    request.send(JSON, strinfigy({username:username, password: password});
 };
